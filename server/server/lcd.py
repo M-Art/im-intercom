23415ad43d4251b3acadc0acfef0094c027c37a6
M-Art <m-art91@tlen.pl>
2013-06-20 19:05:50 +0200

    fix

--- server/server/lcd.py
+++ server/server/lcd.py
@@ -1,8 +1,7 @@
 # coding=UTF-8
 import RPi.GPIO as GPIO
 import time
- 
-#Prze mapowanie pinów RPi na zmienne odpowiadające nazwą pinów wyświetlacza 
+
 LCD_RS = 7
 LCD_E  = 8
 LCD_D4 = 25
@@ -10,15 +9,14 @@ LCD_D5 = 24
 LCD_D6 = 23
 LCD_D7 = 18
  
-# Definicja stałych dla LCD
-LCD_WIDTH = 24    # Maksymalna ilość znaków na linię 
+LCD_WIDTH = 16
 LCD_CHR = True
 LCD_CMD = False
  
-LCD_LINE_1 = 0x80 # LCD RAM adres dla 1 linii
-LCD_LINE_2 = 0xC0 # LCD RAM adres dla 2 linii
+LCD_LINE_1 = 0x80
+LCD_LINE_2 = 0xC0
  
-# Stałe czasowe
+# Time constants
 E_PULSE = 0.00005
 E_DELAY = 0.00005
  
@@ -26,19 +24,16 @@ def init():
     """ Init drivers
     """
     GPIO.setmode(GPIO.BCM) 
-    GPIO.setup(LCD_E, GPIO.OUT)  # E
-    GPIO.setup(LCD_RS, GPIO.OUT) # RS
-    GPIO.setup(LCD_D4, GPIO.OUT) # DB4
-    GPIO.setup(LCD_D5, GPIO.OUT) # DB5
-    GPIO.setup(LCD_D6, GPIO.OUT) # DB6
-    GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+    GPIO.setup(LCD_E, GPIO.OUT)
+    GPIO.setup(LCD_RS, GPIO.OUT)
+    GPIO.setup(LCD_D4, GPIO.OUT)
+    GPIO.setup(LCD_D5, GPIO.OUT)
+    GPIO.setup(LCD_D6, GPIO.OUT)
+    GPIO.setup(LCD_D7, GPIO.OUT)
  
-    # Inicjalizacja wyświetlacza
     __lcd_init()
 
-    # wysłanie tekstu na pierwszą linię
     println(0, "LCD initialized")
-    #wysłanie tekstu nadrugą linię
     println(1, "kotek :)")
 
 def println(line, string):
@@ -52,8 +47,6 @@ def println(line, string):
     else:
         __lcd_byte(LCD_LINE_2, LCD_CMD)
     __lcd_string(string)
- 
-########## Funkcja inicjalizująca wyświetlacz
 
 def __lcd_init():
     __lcd_byte(0x33,LCD_CMD)
@@ -63,17 +56,14 @@ def __lcd_init():
     __lcd_byte(0x06,LCD_CMD)
     __lcd_byte(0x01,LCD_CMD)  
  
-########## Funkcja wysyłająca string na konkretną linię
- 
 def __lcd_string(message):
     message = message.ljust(LCD_WIDTH," ")  
  
     for i in range(LCD_WIDTH):
         __lcd_byte(ord(message[i]),LCD_CHR)
  
-########## Funkcja wysyłająca dane do pinów
 def __lcd_byte(bits, mode):
-    GPIO.output(LCD_RS, mode) # RS
+    GPIO.output(LCD_RS, mode)
  
     # High bits
     GPIO.output(LCD_D4, False)
@@ -89,7 +79,6 @@ def __lcd_byte(bits, mode):
     if bits&0x80==0x80:
         GPIO.output(LCD_D7, True)
  
-    # Przełączenie pinu 6E
     time.sleep(E_DELAY)    
     GPIO.output(LCD_E, True)  
     time.sleep(E_PULSE)
@@ -110,7 +99,6 @@ def __lcd_byte(bits, mode):
     if bits&0x08==0x08:
         GPIO.output(LCD_D7, True)
 
-    # Przełączenie pinu 6E
     time.sleep(E_DELAY)    
     GPIO.output(LCD_E, True)  
     time.sleep(E_PULSE)
